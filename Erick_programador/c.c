bfdd628d84669ab2bfecef2aad9af2be4031f90f
Erick Vitor <erickvitorleo1@gmail.com>
2024-09-24 11:12:36 -0300

    Add files via upload

---
+++ Erick_programador/c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+	int numero1 =2;
+	int numero2 =21;
+	int soma;
+	soma = numero1 + numero2;
+	char letra = '1';
+	char nome[6] = "Erick";
+	printf ("%d \n",soma);
+	printf ("%c \n", letra);
+	printf ("%s", nome);
+	return 0;
+
+}
