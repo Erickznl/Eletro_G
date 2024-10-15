d87745f2954b0e8cdfee1d00a389f885682633e8
Erick Vitor <erickvitorleo1@gmail.com>
2024-10-15 00:29:40 -0300

    Update main.c

--- Erick_programador/main.c
+++ Erick_programador/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
  int main(void) {
-    int comprimento = 400;
-    int altura = 10;
+    int comprimento = 500;
+    int altura = 15;
     int area = altura * comprimento;
     printf("a area sera igual a %d multiplicado por %d e o resultado sera %d", altura, comprimento, area);
-    float preco = 1.6;
+    float preco = 1.7;
     int total = preco * area;
     printf(", o preco sera %f por %d que o seu resultado ira da %d", preco, area, total);
 
